49470458426c8c706ca76d5fc444192851cc5666
Arifcse21 <arif.cse.baust@gmail.com>
2021-02-24 11:58:54 +0600

    json load

--- files/file_JSON.py
+++ files/file_JSON.py
@@ -3,8 +3,5 @@ import json
 file = open('friends_json.txt', 'r')
 file_content = json.load(file)  # Reads file and turns into dict
 file.close()
-print(file_content)
 
-#cars = [
-#    {}
-#]
+print(file_content["friends"][0])
