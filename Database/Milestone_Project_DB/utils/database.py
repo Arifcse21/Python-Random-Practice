2b0d641bc9564017f400811a3f0d1e2f672f21fe
Arifcse21 <arif.cse.baust@gmail.com>
2021-03-12 11:54:42 +0600

    Database Connection with context manager

--- Database/Milestone_Project_DB/utils/database.py
+++ Database/Milestone_Project_DB/utils/database.py
@@ -2,51 +2,40 @@
 Concerned with storing and retrieving data from a Database
 
 """
-import sqlite3
+from .database_connection import DatabaseConnection
 
 
 def create_book_table():
-    conn = sqlite3.connect('data.db')
-    cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS BOOKS(NAME TEXT PRIMARY KEY, AUTHOR TEXT, READ INTEGER)")
-    conn.commit()
-    conn.close()
+    with DatabaseConnection('data.db') as conn:     # Context manager
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS BOOKS(NAME TEXT PRIMARY KEY, AUTHOR TEXT, READ INTEGER)")
 
 
 def add_book(name, author):
-    conn = sqlite3.connect('data.db')
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO books VALUES (?, ?, 0)",  (name, author))
-    conn.commit()
-    conn.close()
+    with DatabaseConnection('data.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO books VALUES (?, ?, 0)",  (name, author))
 
 
 def get_all_books():
-    conn = sqlite3.connect('data.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM books ORDER BY name")
-
-    # books = cursor.fetchall()   # [(name, author, read), (name, author, read)]
-    books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
-    conn.close()
-
+    with DatabaseConnection('data.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM books ORDER BY name")
+        # books = cursor.fetchall()   # [(name, author, read), (name, author, read)]
+        books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
     return books
 
 
 def mark_book_as_read(name):
-    conn = sqlite3.connect('data.db')
-    cursor = conn.cursor()
-    cursor.execute("UPDATE books SET read=1 WHERE name=?",(name,))
-    conn.commit()
-    conn.close()
+    with DatabaseConnection('data.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("UPDATE books SET read=1 WHERE name=?",(name,))
 
 
 def delete_book(name):
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM books where name=?", (name,))
-    conn.commit()
-    conn.close()
+    with DatabaseConnection('data.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM books where name=?", (name,))
 
 
 
